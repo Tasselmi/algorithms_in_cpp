09fd1b53ff9816cca64211c758d48a2948579371
Tasselmi <tasselmi@yeah.net>
2020-06-22 19:45:50 +0800

    stack application

--- chapter08/railroadWithStacks.cpp
+++ chapter08/railroadWithStacks.cpp
@@ -6,10 +6,10 @@
 #include <iostream>
 using namespace std;
 
-ArrayStack<int>* track; // 轨道数组
-int numberOfCars;
+ArrayStack<int>* track; // 轨道数组，每个元素为车厢缓冲栈
+int numberOfCars; //车辆总数
 int numberOfTracks; //缓冲轨道数
-int smallestCar;
+int smallestCar; //编号最小的车
 int itsTrack; //有最小编号的车厢停靠的那个缓冲轨道
 
 void outputFromHoldingTrack();
@@ -58,6 +58,8 @@ void outputFromHoldingTrack()
 
 bool putInHoldingTrack(int c)
 {
+    //track的实际编号是1，2，3，此处令 bestTrack = 0 为初始状态
+    //bestTop 记录的是各条轨道中比插入值大的 [顶点中的最小值]
     int bestTrack = 0, bestTop = numberOfCars + 1;
 
     for (int i = 1; i <= numberOfTracks; ++i) {
@@ -65,11 +67,15 @@ bool putInHoldingTrack(int c)
         if (!track[i].empty()) {
             int topCar = track[i].top();
             cout << "; topCar: " << topCar << "; bestTop: " << bestTop;
-            if (c < topCar && topCar < bestTop) {
+
+            //topCar > c是基本条件，因为每条轨道里面的车厢必须都按照从小到大排列（头部小尾部大）
+            //topCar < bestTop，必须比bestTop小，插入顶点最小的轨道
+            if (topCar > c && topCar < bestTop) {
                 bestTop = topCar;
                 bestTrack = i;
+                cout << "; bestTop-if: " << bestTop << "; bestTrack: " << bestTrack;
             }
-        } else if (bestTrack == 0) {
+        } else if (bestTrack == 0) { //如果 bestTrack 还处于初始状态，那么当前遍历到的首个track即可
             bestTrack = i;
             cout << "; bestTrack: " << bestTrack;
         }
@@ -88,10 +94,10 @@ bool putInHoldingTrack(int c)
     return true;
 }
 
-int main()
-{
-    int p[] = {0, 3, 6, 9, 2, 4, 7, 1, 8, 5};
-    cout << "Input permutation is 369247185" << endl;
-    railroad(p, 9, 3);
-    return 0;
-}
+//int main()
+//{
+//    int p[] = {0, 3, 6, 9, 2, 4, 7, 1, 8, 5};
+//    cout << "Input permutation is 369247185" << endl;
+//    railroad(p, 9, 3);
+//    return 0;
+//}
---
+++ chapter08/switchBox.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Apple on 2020/6/22.
+//
+
+#include "ArrayStack.h"
+#include <iostream>
+using namespace std;
+
+/**
+ * 开关盒是否可以按照设计进行布线
+ * @param net 将针脚编号按照从小到大排列后的 【组标】 数列
+ * @param n 针脚个数
+ * @return 是否可布线
+ */
+bool checkBox(const int net[], int n)
+{
+    auto *s = new ArrayStack<int>(n);
+
+    for (int i = 0; i < n; ++i) {
+        if (!s->empty()) {
+            if (net[i] == net[s->top()]) { //当前待处理的组标与栈顶组标相等时才能匹配；
+                s->pop();
+            }
+            else s->push(i);
+        } else {
+            s->push(i);
+        }
+    }
+
+    if (s->empty()) {
+        cout << "switch box is routable" << endl;
+        return true;
+    } else {
+        cout << "switch box is not routable" << endl;
+        return false;
+    }
+}
+
+int main()
+{
+    //组标数列，两两一组 {1, 2, 3, 4, 5, 6, 7, 8} --> (1, 4) (2, 3) (5, 6) (7, 8)
+    int net[] = {1, 2, 2, 1, 3, 3, 4, 4};
+    checkBox(net, 8);
+
+    //组标数列 {1, 2, 3, 4, 5, 6, 7, 8} --> (1, 8) (2, 5) (3, 6) (4, 7)
+    int net2[] = {1, 2, 3, 4, 2, 3, 4, 1};
+    checkBox(net2, 8);
+
+    //组标数列 {1, 2, 3, 4} --> (1, 3) (2, 4)
+    int net4[] = {1, 2, 1, 2};
+    checkBox(net4, 4);
+    return 0;
+}
