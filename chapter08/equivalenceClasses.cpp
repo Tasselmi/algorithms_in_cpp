44f378d7813ba7baeb8eb97f8432b4ee0948f18d
Tasselmi liang <liang@TasselmideiMac-Pro.local>
2020-06-24 01:36:07 +0800

    add maze using stack

--- chapter08/equivalenceClasses.cpp
+++ chapter08/equivalenceClasses.cpp
@@ -1,93 +1,93 @@
+////
+//// Created by Apple on 2020/6/23.
+////
 //
-// Created by Apple on 2020/6/23.
+//#include "ArrayStack.h"
+//#include <utility>
+//#include <array>
 //
-
-#include "ArrayStack.h"
-#include <utility>
-#include <array>
-
-int main()
-{
-    const int n = 22; //22个元素
-    const int r = 11; //11个数对
-
-    using pint = std::pair<int, int>;
-    array<pint, r> pairs; //11个数对
-    pairs[0] = pint(1, 5);
-    pairs[1] = pint(1, 6);
-    pairs[2] = pint(3, 7);
-    pairs[3] = pint(4, 8);
-    pairs[4] = pint(5, 2);
-    pairs[5] = pint(6, 5);
-    pairs[6] = pint(4, 9);
-    pairs[7] = pint(9, 7);
-    pairs[8] = pint(7, 8);
-    pairs[9] = pint(3, 4);
-    pairs[10] = pint(6, 2);
-
-    int theMax = 0;  //所有元素的最大值
-    auto list = new ArrayStack<int>[n + 1]; //下标为0的 ArrayStack 不使用，从1开始使用
-    for (auto x : pairs) {
-        int s = x.first, t = x.second;
-        std::cout << s << ", " << t << endl;
-        list[s].push(t);
-        list[t].push(s);
-
-        if (s > theMax) { theMax = s; }
-        if (t > theMax) { theMax = t; }
-    }
-    std::cout << "theMax: " << theMax << endl;
-    std::cout << "\n------------------\n";
-
-    for (int k = 1; k <= theMax; ++k) {
-        std::cout << k << "th stack: ";
-        while (!list[k].empty()) {
-            std::cout << list[k].top() << " ";
-            list[k].pop();
-        }
-        std::cout << endl;
-    }
-    std::cout << "\n------------------\n";
-
-    //前面为了打印每个栈，将栈遍历清空了，因此需要重新插入1次
-    for (auto x : pairs) {
-        int s = x.first, t = x.second;
-        list[s].push(t);
-        list[t].push(s);
-    }
-
-    //unprocessedList存储的是尚未被检索到的栈的编号
-    ArrayStack<int> unprocessedList;
-    // out的作用其实是一个标志位的作用，有输出过的不再重复输出
-    array<bool, n + 1> out = {true, false}; //这个地方也可以不初始化
-    for (auto &x : out) { x = false; } //引用类型可以修改源值
-
-    for (int i = 1; i <= theMax; ++i) {
-        if (!out[i]) {
-            std::cout << i << " ";
-            out[i] = true;
-            unprocessedList.push(i);
-
-            while (!unprocessedList.empty()) { //第一层，每个编号为1th~9th的栈被处理到
-                int j = unprocessedList.top();
-                unprocessedList.pop();
-                while (!list[j].empty()) { //第二层，循环穷尽栈内每个元素
-                    int q = list[j].top();
-                    list[j].pop();
-                    if (!out[q]) {
-                        std::cout << q << " ";
-                        out[q] = true;
-                        //q是栈内的元素（已被处理），但是第q个栈还没处理到，所以需要添加到栈编号中去
-                        //虽然最外层的 i++ 循环中也能确保第 i th 个栈被处理到，但是这里是寻找等价类
-                        //因此所有的相关元素必须在一次i循环体中处理完毕，否则元素就错误的划分到另外的等价类中去了
-                        unprocessedList.push(q);
-                    }
-                }
-            }
-
-            std::cout << endl;
-        }
-    }
-
-    return 0;
-}
+//int main()
+//{
+//    const int n = 22; //22个元素
+//    const int r = 11; //11个数对
+//
+//    using pint = std::pair<int, int>;
+//    array<pint, r> pairs; //11个数对
+//    pairs[0] = pint(1, 5);
+//    pairs[1] = pint(1, 6);
+//    pairs[2] = pint(3, 7);
+//    pairs[3] = pint(4, 8);
+//    pairs[4] = pint(5, 2);
+//    pairs[5] = pint(6, 5);
+//    pairs[6] = pint(4, 9);
+//    pairs[7] = pint(9, 7);
+//    pairs[8] = pint(7, 8);
+//    pairs[9] = pint(3, 4);
+//    pairs[10] = pint(6, 2);
+//
+//    int theMax = 0;  //所有元素的最大值
+//    auto list = new ArrayStack<int>[n + 1]; //下标为0的 ArrayStack 不使用，从1开始使用
+//    for (auto x : pairs) {
+//        int s = x.first, t = x.second;
+//        std::cout << s << ", " << t << endl;
+//        list[s].push(t);
+//        list[t].push(s);
+//
+//        if (s > theMax) { theMax = s; }
+//        if (t > theMax) { theMax = t; }
+//    }
+//    std::cout << "theMax: " << theMax << endl;
+//    std::cout << "\n------------------\n";
+//
+//    for (int k = 1; k <= theMax; ++k) {
+//        std::cout << k << "th stack: ";
+//        while (!list[k].empty()) {
+//            std::cout << list[k].top() << " ";
+//            list[k].pop();
+//        }
+//        std::cout << endl;
+//    }
+//    std::cout << "\n------------------\n";
+//
+//    //前面为了打印每个栈，将栈遍历清空了，因此需要重新插入1次
+//    for (auto x : pairs) {
+//        int s = x.first, t = x.second;
+//        list[s].push(t);
+//        list[t].push(s);
+//    }
+//
+//    //unprocessedList存储的是尚未被检索到的栈的编号
+//    ArrayStack<int> unprocessedList;
+//    // out的作用其实是一个标志位的作用，有输出过的不再重复输出
+//    array<bool, n + 1> out = {true, false}; //这个地方也可以不初始化
+//    for (auto &x : out) { x = false; } //引用类型可以修改源值
+//
+//    for (int i = 1; i <= theMax; ++i) {
+//        if (!out[i]) {
+//            std::cout << i << " ";
+//            out[i] = true;
+//            unprocessedList.push(i);
+//
+//            while (!unprocessedList.empty()) { //第一层，每个编号为1th~9th的栈被处理到
+//                int j = unprocessedList.top();
+//                unprocessedList.pop();
+//                while (!list[j].empty()) { //第二层，循环穷尽栈内每个元素
+//                    int q = list[j].top();
+//                    list[j].pop();
+//                    if (!out[q]) {
+//                        std::cout << q << " ";
+//                        out[q] = true;
+//                        //q是栈内的元素（已被处理），但是第q个栈还没处理到，所以需要添加到栈编号中去
+//                        //虽然最外层的 i++ 循环中也能确保第 i th 个栈被处理到，但是这里是寻找等价类
+//                        //因此所有的相关元素必须在一次i循环体中处理完毕，否则元素就错误的划分到另外的等价类中去了
+//                        unprocessedList.push(q);
+//                    }
+//                }
+//            }
+//
+//            std::cout << endl;
+//        }
+//    }
+//
+//    return 0;
+//}
---
+++ chapter08/maze.cpp
@@ -0,0 +1,123 @@
+//
+// Created by Tasselmi liang on 2020/6/23.
+//
+
+#include <new>
+#include "ArrayStack.h"
+using namespace std;
+
+struct position
+{
+    int row;
+    int col;
+    explicit operator int() const { return row; }
+    position(int x, int y) { row = x; col = y; }
+    position() = default;
+};
+
+//create a two dimensional array
+template<typename T>
+bool make2dArray(T ** &x, int numberOfRows, int numberOfColumns)
+{
+    try {
+        x = new T* [numberOfRows];
+        for (int i = 0; i < numberOfRows; ++i) {
+            x[i] = new int [numberOfColumns];
+        }
+        return true;
+    } catch (bad_alloc&) {
+        return false;
+    }
+}
+
+int **maze, size;
+ArrayStack<position> * path;
+
+void inputMaze()
+{
+    cout << "enter maze size" << endl;
+    cin >> size;
+    make2dArray(maze, size + 2, size + 2);
+    cout << "Enter maze in row major order" << endl;
+
+    for (int i = 1; i <= size; ++i) {
+        for (int j = 1; j <= size ; ++j) {
+            cin >> maze[i][j];
+        }
+    }
+}
+
+bool findPath()
+{
+    path = new ArrayStack<position>;
+
+    position offset[4];
+    offset[0].row = 0; offset[0].col = 1;
+    offset[1].row = 1; offset[1].col = 0;
+    offset[2].row = 0; offset[2].col = -1;
+    offset[3].row = -1; offset[3].col = 0;
+
+    //外围的障碍墙
+    for (int i = 0; i <= size + 1; ++i) {
+        maze[0][i] = maze[size + 1][i] = 1; //第一行和最后一行
+        maze[i][0] = maze[i][size + 1] = 1; //第一列和最后一列
+    }
+
+    position here(1, 1);
+    maze[1][1] = 1;
+    int option = 0;
+    int lastOption = 3;
+
+    while (here.row != size || here.col != size) {
+        int r, c;
+
+        while (option <= lastOption) {
+            r = here.row + offset[option].row;
+            c = here.col + offset[option].col;
+            if (maze[r][c] == 0) break; //如果找到了下一步的位置，那么停止继续查找
+            option++;
+        }
+
+        if (option <= lastOption) {
+            path->push(here);
+            here.row = r;
+            here.col = c;
+            maze[r][c] = 1;
+            option = 0;
+        } else {
+            if (path->empty()) return false;
+            position previous = path->top();
+            path->pop();
+            // next.col - here.col = +1 或 -1
+            // 行相等说明之前的一步是左右移动的（前一步option=0或2），那么从其后一步继续移动即可，也即option=1或3
+            if (previous.row == here.row) option = 2 + previous.col - here.col;
+            //列相等说明之前的一步是上下移动的（前一步option=1或3），那么从其后一步继续移动即可，也即option=2或4
+            //option=2 可以继续进行，如果 option=4 那么 option>lastOption，会进入本else分支继续退栈
+            else option = 3 + previous.row - here.row;
+            here = previous;
+        }
+    }
+
+    return true;
+}
+
+void outputPath()
+{// Output path to exit.
+    cout << "The path is" << endl;
+    position here;
+    while (!path->empty())
+    {
+        here = path->top();
+        path->pop();
+        cout << here.row << ' ' << here.col << endl;
+    }
+}
+
+int main()
+{
+    inputMaze();
+    if (findPath())
+        outputPath();
+    else
+        cout << "No path" << endl;
+}
